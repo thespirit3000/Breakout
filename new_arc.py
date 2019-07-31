41338140ada830218926a5e5b2a543629187403f
Denis Bogdan <dbogdan@localhost.localdomain>
2019-07-31 22:07:15 +0300

    next_change

--- new_arc.py
+++ new_arc.py
@@ -3,15 +3,7 @@ import math
 
 #pygame.init()
 
-(width, height) = (480, 600)
-screen = pygame.display.set_mode((width, height))
-pygame.display.set_caption('My arcanoid game')
-#Colours
-red = (255,0,0)
-light_blue = (52,153,255)
-background_colour = (255,255,255)
-
-#Functions
+#_________Functions________
 def collide(p1, p2):
     # center of brick
     dx = p1.x - (p2.x + (p2.width//2))
@@ -22,20 +14,18 @@ def collide(p1, p2):
 
         p1.x += math.sin(angle)
         p1.y -= math.cos(angle)   
-    """ dx = p1.x - p2.x
-    dy = p1.y - p2.y
 
-    dist = math.hypot(dx, dy)
-    if dist < p1.size:
-        tangent = math.atan2(dy, dx)
-        angle = 0.5 * math.pi + tangent
 
-        angle1 = 2*tangent - p1.angle
+#_________Classes________
+class Game:
+    def __init__(self, blocks, paddle, ball):
+        self.blocks = blocks
+        self.ball = ball
+        self.paddle = paddle
 
-        p1.x += math.sin(angle)
-        p1.y -= math.cos(angle) """
+        
+                
 
-#Classes
 class ball(object):
     def __init__(self,x,y, size):
         self.x = x
@@ -55,73 +45,104 @@ class ball(object):
         self.y -= math.cos(self.angle) * self.speed
     
     def bounce(self):
-        if self.x > width - self.size:
-            self.x = 2*(width - self.size) - self.x
+        if self.x > WIDTH - self.size:
+            self.x = 2*(WIDTH - self.size) - self.x
             self.angle = - self.angle
 
         elif self.x < self.size:
             self.x = 2*self.size - self.x
             self.angle = - self.angle
 	 
-        if self.y > height - self.size:
+        if self.y > HEIGHT - self.size:
             print('life minus')
-            self.y = 2*(height - self.size) - self.y
+            self.y = 2*(HEIGHT - self.size) - self.y
             self.angle = math.pi - self.angle	 
 
         elif self.y < self.size:
             self.y = 2*self.size - self.y
             self.angle = math.pi - self.angle
-        
-    
-        
-
-
-class enemy(object):
-    def __init__(self, x, y, width, height):
+   
+class block(object):
+    def __init__(self, x, y):
         self.x = x
         self.y = y
-        self.width = width
-        self.height = height
-        
-
+        self.width = BLOCK_WIDTH
+        self.height = BLOCK_HEIGHT
+    
     def draw(self, win):
         rect = (self.x, self.y, self.width, self.height)
         pygame.draw.rect(win, red, rect)
-    
-game_ball = ball(150, 1, 10)
-enemy_bricks = []
-n_of_rows, n_in_row = 5, 10
-init_x = 0
-init_y = 500
-row = []
 
-for i in range(n_of_rows):
-    for m in range(n_in_row):
-        row.append(enemy(init_x,init_y,(width-n_in_row)//n_in_row,20))
-        init_x += (width//n_in_row)
-    enemy_bricks.append(row)
-    init_x = 0
-    init_y += 22
+class paddle(object):
+    def __init__(self, x, y, speed):
+        self.x = x
+        self.y = y
+        self.width = PADDLE_WIDTH
+        self.height = 10
+        self.speed = PADDLE_SPEED
+
+    def draw(self,win):
+        rect = (self.x, self.y, self.width, self.height)
+        pygame.draw.rect(win, red, rect)
+
+    def move(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT] and self.x > VEL:
+            self.x -= self.speed
+            
+        elif keys[pygame.K_RIGHT] and self.x < WIDTH - self.width - VEL:
+            self.x += self.speed
+            
+#_________Config________
+WIDTH = 500
+HEIGHT = 600
+FPS = 60
+BALL_SPEED = 20
+PADDLE_WIDTH = 50
+PADDLE_SPEED = 3
+BLOCK_WIDTH = 50
+BLOCK_HEIGHT = 20
+QTY_ROWS = 5
+QTY_COLUMNS = 10
+VEL = 5
+
+#_________Colours________
+red = (255,0,0)
+light_blue = (52,153,255)
+background_colour = (255,255,255)
+game_paddle = paddle((WIDTH//2) - (PADDLE_WIDTH//2), HEIGHT - 14, 1)
+
+
+#_________Init________
+pygame.init()
+clock = pygame.time.Clock()
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption('My arcanoid game')
+game_ball = ball(150, 1, 10)
+bricks = []
+
+for y in range(0,BLOCK_HEIGHT*QTY_ROWS, BLOCK_HEIGHT+1):
+    for x in range(2, WIDTH, BLOCK_WIDTH+1):
+        bricks.append(block(x, y))
+
 
 running = True
+#_________Main loop________
 while running:
-
+    clock.tick(FPS)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    screen.fill(background_colour)
-    #enemy_brick.draw(screen)
     
-    for row in enemy_bricks:
-        for enemy in row:
-            enemy.draw(screen)
-            
-            collide(game_ball, enemy)
-            
+    screen.fill(background_colour)
+    for brick in bricks:
+        brick.draw(screen)
     game_ball.move()
     game_ball.bounce()
     game_ball.draw(screen)
+    game_paddle.draw(screen)
+    game_paddle.move()
     pygame.display.flip()
 
 pygame.quit()
